107f6083b15ccf2acc572ee53a5118eaf2a98f70
Damian Molinski <tedeer@gmail.com>
2021-08-15 15:21:25 +0200

    Work with opened mobile menu

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const mobileMenuButton = document.querySelector("#mobile-menu-button");
+const mobileMenu = document.querySelector(".mobile-menu");
+const mobileMenuOpen = document.querySelector(".mobile-menu-open");
+const body = document.body;
+
+mobileMenuButton.addEventListener("click", function () {
+    mobileMenu.classList.toggle("mobile-menu");
+    mobileMenu.classList.toggle("mobile-menu-open");
+    console.log("lol");
+    console.log(mobileMenu.classList);
+});
