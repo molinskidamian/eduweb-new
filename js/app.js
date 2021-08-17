45c4623ee6c6a8ee4e9316dec1ae46e2d5ed95b9
Damian Molinski <tedeer@gmail.com>
2021-08-17 13:32:01 +0200

    Continued work with mobile-nav-extends

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
-const mobileMenuButton = document.querySelector("#mobile-menu-button");
-const mobileMenu = document.querySelector(".mobile-menu");
-const mobileMenuOpen = document.querySelector(".mobile-menu-open");
-const body = document.body;
+(function () {
+    /* Mobile menu */
+    const mobileNavExtends = document.querySelector(".mobile-main-nav-extends");
+    const mobileNav = document.querySelector(".mobile-main-nav");
+    const mobileMenuButton = document.querySelector("#mobile-menu-trigger");
 
-mobileMenuButton.addEventListener("click", function () {
-    mobileMenu.classList.toggle("mobile-menu");
-    mobileMenu.classList.toggle("mobile-menu-open");
-    document.querySelector(".mobile-menu-extends").style.display = "flex";
-    console.log("lol");
-    console.log(mobileMenu.classList);
-});
+    mobileMenuButton.addEventListener("click", function () {
+        console.log("pokaz");
+        mobileNavExtends.classList.toggle("mobile-nav--show");
+        mobileMainNav = document.querySelector(".mobile-main-nav");
+        mobileMainNav.classList.toggle("mobile-nav--show");
+        // mobileMainNavWrapper.style.display = "none";
+    });
+})();
