a8b4546ba5b542a7810772364571a8ea2abe49c2
Damian Molinski <tedeer@gmail.com>
2023-01-18 00:04:22 +0100

    Work with header form search

--- js/main-menu.js
+++ js/main-menu.js
@@ -1,19 +1,29 @@
-(function() {
-    const elements = document.querySelectorAll(".main_nav--link");
-    const newElements = [];
+(function () {
+	//Main menu
+	const elements = document.querySelectorAll('.main_nav--link');
+	const newElements = [];
 
-    for (let i = 0; i < elements.length; i++) {
-        newElements.push(elements[i]);
-        console.log(elements[i]);
+	for (let i = 0; i < elements.length; i++) {
+		newElements.push(elements[i]);
+		console.log(elements[i]);
 
-        elements[i].addEventListener("mouseover", (e) => {
-            e.target.classList.add("is-hover");
-            console.log(e.target);
-        });
+		elements[i].addEventListener('mouseover', e => {
+			e.target.classList.add('is-hover');
+			console.log(e.target);
+		});
 
-        elements[i].addEventListener("mouseout", (e) => {
-            e.target.classList.remove("is-hover");
-            console.log(e.target);
-        });
-    }
+		elements[i].addEventListener('mouseout', e => {
+			e.target.classList.remove('is-hover');
+			console.log(e.target);
+		});
+	}
+
+	// Main search
+	const input = document.querySelector('.search-field');
+	const buttonSearch = document.querySelector('.search-btn');
+
+	input.addEventListener('focus', () => {
+		console.log('focus');
+		buttonSearch.classList.add('is-focus');
+	});
 })();
